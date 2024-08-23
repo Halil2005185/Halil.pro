1955e80c981a922be15d93d1b5ae719414aa2124
Halil2005185 <ismailhalil073@gmail.com>
2024-08-23 13:42:32 +0300

    Add files via upload

---
+++ javatst.js
@@ -0,0 +1,28 @@
+console.log((15.56789).toFixed(2));
+console.log(parseInt("100 Students"));
+console.log((45.67).toFixed(0),"%");
+console.log(Math.round(14.8 ));
+console.log(Math.round(14.3));
+console.log(Math.ceil(5.3 ));
+console.log(Math.ceil(5.9));
+console.log(Math.floor(7.9 ));
+console.log(Math.min(15,2,10,7,3));
+console.log(Math.max(15,2,10,7,3));
+console.log(Math.pow(2,4));
+console.log(Math.pow(5,3));
+console.log(Math.random(0)*1);
+console.log(Math.random(1)*100);
+console.log(Math.random(50)*150);
+console.log((100).toString());
+
+
+
+
+
+
+
+
+
+
+
+
